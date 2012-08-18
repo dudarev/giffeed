76238052deea6f51f94e40739718c962dea4e171
Artem Dudarev <dudarev@gmail.com>
2012-08-18 15:50:29 -0500

    tests for parsing

--- giffeed/bots/tests.py
+++ giffeed/bots/tests.py
@@ -7,10 +7,25 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from giffeed.bots.models import Bot
+
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_url_parsing_duplicates(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that gif links are parsed correctly without duplicates.
         """
-        self.assertEqual(1 + 1, 2)
+
+        b = Bot(name='_test', url='http://example.com')
+        links = b.parse_url(file_name='giffeed/bots/test_data/gif_parsing.txt')
+        self.assertEqual(len(links), 4)
+
+    def test_url_parsing_ignore(self):
+        """
+        Tests that for source 'topsy' all links that have topsy are ignored.
+        """
+
+        b = Bot(name='_test', url='http://example.com', source='topsy')
+        links = b.parse_url(file_name='giffeed/bots/test_data/gif_parsing.txt')
+        for l in links:
+            self.assertFalse('topsy' in l)
