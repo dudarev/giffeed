92b0b7ee351b128361b95f4a6e061987cf3f3e10
Artem Dudarev <dudarev@gmail.com>
2012-08-18 15:53:06 -0500

    new source field, improved parsing

--- giffeed/bots/models.py
+++ giffeed/bots/models.py
@@ -9,13 +9,30 @@ class Bot(models.Model):
     url = models.URLField()
     # time in past for new bots
     last_run_at = models.DateTimeField(default=datetime.datetime(2000,1,1))
+    source = models.CharField(max_length=30, null=True)
 
     def __unicode__(self):
         return self.name
 
-    def parse_url(self):
-        url_request = urllib2.Request(self.url)
-        response = urllib2.urlopen(url_request)
-        feed = response.read()
-        gif_regex = re.compile('http[^\'" >]+.gif', re.IGNORECASE)
-        return re.findall(gif_regex, feed)
+    def parse_url(self, file_name=None):
+
+        if file_name:
+            feed = open(file_name, 'r').read()
+        else:
+            url_request = urllib2.Request(self.url)
+            response = urllib2.urlopen(url_request)
+            feed = response.read()
+
+        gif_regex = re.compile('http[^\'" \n>]+.gif', re.IGNORECASE)
+        links = set(re.findall(gif_regex, feed))
+
+        # special cases
+
+        # ignore links with topsy from topsy
+        if self.source == 'topsy':
+            links = [l for l in links if not 'topsy' in l]
+        else:
+            links = list(links)
+
+        return links
+
