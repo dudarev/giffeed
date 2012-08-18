b4bd78b77ba304d54987d0cf431f411b16fae6fe
Artem Dudarev <dudarev@gmail.com>
2012-08-18 10:56:44 +0300

    main page

--- giffeed/urls.py
+++ giffeed/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic import TemplateView
+
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
+
     # Examples:
-    # url(r'^$', 'giffeed.views.home', name='home'),
     # url(r'^giffeed/', include('giffeed.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
