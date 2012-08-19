38459d984330101c48d9bc95f4489d5d7a4d0c84
Artem Dudarev <dudarev@gmail.com>
2012-08-19 16:24:47 +0300

    added s3_folder_storage for static and media

--- giffeed/settings.py
+++ giffeed/settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
 
     'south',
     'gunicorn',
+    's3_folder_storage',
 
     'giffeed.core',
     'giffeed.bots',
@@ -149,6 +150,22 @@ LOGGING = {
         },
     }
 }
+
+# s3_folder_storage
+
+DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+DEFAULT_S3_PATH = "media"
+STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+STATIC_S3_PATH = "static"
+AWS_ACCESS_KEY_ID = 'AKIAIIXIHHARNJLKPM5Q'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'giffeed'
+
+MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
+MEDIA_URL = '//s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
+STATIC_ROOT = "/%s/" % STATIC_S3_PATH
+STATIC_URL = '//s3.amazonaws.com/%s/static/' % AWS_STORAGE_BUCKET_NAME
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 try:
     from local_settings import *
