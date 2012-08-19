15cc4dbecc6dc323eb361ecfe2fc248a94e354cc
Artem Dudarev <dudarev@gmail.com>
2012-08-19 11:33:20 -0500

    modified Post model

--- giffeed/core/models.py
+++ giffeed/core/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     user = models.ForeignKey(User)
-    gif_url = models.URLField(null=True)
+    gif_url = models.URLField(null=True, unique=True)
+    time_added = models.DateTimeField(auto_now_add=True)
