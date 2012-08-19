22f63bb3825401f2e70ef35e11c29e9584a82e8a
Artem Dudarev <dudarev@gmail.com>
2012-08-20 01:53:44 +0300

    about page

--- giffeed/urls.py
+++ giffeed/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,7 @@ from giffeed.core.views import HomePageView, UserPageView
 
 urlpatterns = patterns('',
     url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^about$', TemplateView.as_view(template_name="about.html"), name='about'),
     url(r'^(?P<username>[^/]+)$', UserPageView.as_view(), name='user'),
     url(r'^admin/', include(admin.site.urls)),
 )
