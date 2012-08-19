3b2b893234a16b510155ec353a5dd9eba17ad5cf
Artem Dudarev <dudarev@gmail.com>
2012-08-19 16:58:34 -0500

    started page for user

--- giffeed/core/views.py
+++ giffeed/core/views.py
@@ -3,6 +3,8 @@ from django.views.generic.base import TemplateView
 from django.db.models import Max
 from django.contrib.auth.models import User
 
+from giffeed.core.models import Post
+
 class HomePageView(TemplateView):
 
     template_name = "home.html"
@@ -18,4 +20,18 @@ class HomePageView(TemplateView):
                 pass
 
         context['latest_posts'] = posts
+        return context
+
+class UserPageView(TemplateView):
+
+    template_name = "user.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(UserPageView, self).get_context_data(**kwargs)
+
+        username = kwargs['username']
+        posts = Post.objects.all().filter(user=username)[:10]
+
+        context['latest_posts'] = posts
+        context['username'] = username 
         return context
--- giffeed/urls.py
+++ giffeed/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from giffeed.core.views import HomePageView
+from giffeed.core.views import HomePageView, UserPageView
 
 urlpatterns = patterns('',
     url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^(?P<username>[^/]+)$', UserPageView.as_view(), name='user'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
