d4a28883c3c7b28fdb8023107ef75b10cbbf1d9b
Artem Dudarev <dudarev@gmail.com>
2012-08-19 10:19:58 -0500

    removed GIF model for now

--- giffeed/core/models.py
+++ giffeed/core/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class GIF(models.Model):
-    url = models.URLField()
-
 class Post(models.Model):
     user = models.ForeignKey(User)
-    gif = models.ForeignKey(GIF)
+    gif_url = models.URLField(null=True)
