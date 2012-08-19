e2936f0f71b4ac49a724260ec3c8ef5edc6cc200
Artem Dudarev <dudarev@gmail.com>
2012-08-19 05:18:27 -0500

    started GIF and post models

--- giffeed/core/models.py
+++ giffeed/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class GIF(models.Model):
+    url = models.URLField()
+
+class Post(models.Model):
+    user = models.ForeignKey(User)
+    gif = models.ForeignKey(GIF)
