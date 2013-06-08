56d210633b001c58ec90cce88ed9334f18edeeee
Dmitriy Zavalkin <dmitriy.zavalkin@gmail.com>
2013-06-08 23:26:23 +0200

    implemented social auth via twitter

--- giffeed/urls.py
+++ giffeed/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from .views import LoginView, LoginErrorView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,5 +13,9 @@ urlpatterns = patterns('',
     url(r'^update$', 'giffeed.bots.views.update', name='update'),
     url(r'^(?P<username>[^/]+)$', UserPageView.as_view(), name='user'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('social_auth.urls')),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^login-error/$', LoginErrorView.as_view(), name='login-error'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout')
 )
 
