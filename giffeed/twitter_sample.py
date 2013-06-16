d0cf2387b1104399a8d06be0a42ee09ef7c40550
nastya <nastya.zavalkina@gmail.com>
2013-06-16 19:55:18 +0200

    Created a template to show search results, added some changes to make code PEP8 compatible.

---
+++ giffeed/twitter_sample.py
@@ -0,0 +1,5 @@
+#Rename to twitter.py and get values from https://dev.twitter.com/apps
+TWITTER_CONSUMER_KEY = ''
+TWITTER_CONSUMER_SECRET = ''
+TWITTER_ACCESS_TOKEN = ''
+TWITTER_ACCESS_TOKEN_SECRET = ''
--- giffeed/views.py
+++ giffeed/views.py
@@ -1,74 +1,91 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
-# AD: TODO: not good practice in accordance with PEP8. Mention what function and classes used in the code explicitly.
+from django.template.context import RequestContext
+from django.views.generic import TemplateView
+# AD: TODO: not good practice in accordance with PEP8.
+# Mention what function and classes used in the code explicitly.
 from TwitterSearch import *
-from giffeed import settings
+from giffeed import settings, twitter
 
 
 def search_form(request):
     return render(request, 'search_form.html')
 
 
+def extract_urls(tweets):
+    """
+    takes tweets and returns a list of urls that contain gifs
+    """
+    extracted_urls = []
+    results_count = 0  # result is something that contains a gif
+    tweet_count = 0  # tweets in general
+    message = ''
+    for tweet in tweets:
+        if 'entities' in tweet:
+            if 'urls' in tweet['entities'] and tweet['entities']['urls'] != []:
+                for element in tweet['entities']['urls']:
+                    if element['expanded_url'].lower().endswith('.gif'):
+                        if element['expanded_url'] not in extracted_urls:
+                            extracted_urls.append(element['expanded_url'])
+                            results_count += 1
+        tweet_count += 1
+        if tweet_count > (settings.total_tweets_max - 1):
+            message += "Number of tweets: {} Number of results with gifs: {}".format(
+                tweet_count, results_count)
+            break
+        else:
+            print len(extracted_urls)
+    return extracted_urls
+
+
 def search(request):
+    """
+
+    :param request:
+    :return:
+    """
     if 'q' in request.GET:
-        # AD: NOTE: let's also tweets that have the keyword and a word `gif` in them
-        search_request = (request.GET['q']+" gif").lower()
+        # AD: NOTE: let's also tweets that have the keyword and a word
+        # `gif` in them
+        search_request = (request.GET['q'] + " gif").lower()
         try:
-            tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-            tso.setKeywords([search_request]) # let's define all words we would like to have a look for
-            # AD: TODO: move this to settings
-            tweets_per_page = 50
-            tso.setCount(tweets_per_page) # please dear Mr Twitter, only give us results_per_page results per page
-            tso.setIncludeEntities(True) # and don't give us all those entity information
+            tso = TwitterSearchOrder()  # create a TwitterSearchOrder object
+            tso.setKeywords([search_request])  # define search request
+            tso.setCount(settings.tweets_per_page)  # only give results_per_page
+            tso.setIncludeEntities(True)  # give us entity information
 
-            # it's about time to create a TwitterSearch object with our secret tokens
+            # create a TwitterSearch object with our secret tokens
             ts = TwitterSearch(
-                consumer_key = settings.TWITTER_CONSUMER_KEY,
-                consumer_secret = settings.TWITTER_CONSUMER_SECRET,
-                access_token = settings.TWITTER_ACCESS_TOKEN,
-                access_token_secret = settings.TWITTER_ACCESS_TOKEN_SECRET
-             )
+                consumer_key=twitter.TWITTER_CONSUMER_KEY,
+                consumer_secret=twitter.TWITTER_CONSUMER_SECRET,
+                access_token=twitter.TWITTER_ACCESS_TOKEN,
+                access_token_secret=twitter.TWITTER_ACCESS_TOKEN_SECRET
+            )
 
-            ts.authenticate() # we need to use the oauth authentication first to be able to sign messages
+            ts.authenticate()  # user must authenticate first
 
+            tweets = ts.searchTweetsIterable(tso)
             found_urls = []
-            info = ''
-            results_count = 0  # result is something that contains a gif
-            tweet_count = 0  # tweets in general
-            # AD: TODO: move to settings (?), maximum number of tweets to get 
-            # it may be larger than tweets_per_page
-            total_tweets_max = tweets_per_page
-            message = ''
-
-            for tweet in ts.searchTweetsIterable(tso):  # this is where the fun actually starts :)
-                if 'entities' in tweet:
-                     if 'urls' in tweet['entities'] and tweet['entities']['urls'] != []:
-                         for element in tweet['entities']['urls']:
-                             print element['expanded_url']
-                             if element['expanded_url'].lower().endswith('.gif'):
-                                 if element['expanded_url'] not in found_urls:
-                                     info += '<img src="{}">'.format(element['expanded_url'])
-                                     found_urls.append(element['expanded_url'])
-                                     results_count += 1
-                tweet_count += 1
-                if tweet_count > (total_tweets_max - 1):
-                    message += "Number of tweets: {} Number of results with gifs: {}".format(
-                        tweet_count, results_count)
+            while True:
+                found_urls += extract_urls(tweets)
+                if len(found_urls) >= 50:
                     break
-            else:
-                # AD: TODO: this is probably should be removed, it is here now for debugging
-                if tweet_count == 0:
-                    message = "Zero tweets were found."
                 else:
-                    message = "Number of tweets: {} Number of results with gifs: {}".format(
-                        tweet_count, results_count)
-            print len(found_urls)
-            # AD: TODO: do this with template
-            html = "<html><body><ul>{}</ul><p>{}</p></body></html>".format(info, message)
-            return HttpResponse(html)
+                    tweets = ts.searchNextResults()
 
-        except TwitterSearchException, e: # take care of all those ugly errors if there are some
+            context = RequestContext(request)
+            context['found_urls'] = found_urls
+            context['search_request'] = search_request
+
+            return render_to_response('search_results.html',
+                                      context_instance=context)
+
+        except TwitterSearchException, e:  # to take care of errors
             message = e.message
     else:
         message = 'You submitted an empty form.'
     return HttpResponse(message)
+
+
+class SearchResultsView(TemplateView):
+    template_name = "search_results.html"
